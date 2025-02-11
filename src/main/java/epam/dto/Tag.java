ff691217ecac805e3723950a99882b05f4272374
pavanichilukala <73467155+pavanichilukala@users.noreply.github.com>
2025-02-11 22:37:06 +0530

    Create src/main/java/epam/dto/Tag.java

---
+++ src/main/java/epam/dto/Tag.java
@@ -0,0 +1,15 @@
+package epam.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+    private Long id;
+    private String name;
+}
