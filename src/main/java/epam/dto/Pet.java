07719e167d3b1b17ca8f4a65445bcbc5e3a42201
pavanichilukala <73467155+pavanichilukala@users.noreply.github.com>
2025-02-11 22:20:08 +0530

    Create src/main/java/epam/dto/Pet.java

---
+++ src/main/java/epam/dto/Pet.java
@@ -0,0 +1,19 @@
+package epam.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pet {
+    private Long id;
+    private Category category;
+    private String name;
+    private List<String> photoUrls;
+    private List<Tag> tags;
+    private String status;
+}
