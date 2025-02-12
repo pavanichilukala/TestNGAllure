dd79760d5c163816563920f5dbd3420d9eaede23
pavanichilukala <73467155+pavanichilukala@users.noreply.github.com>
2025-02-12 12:06:03 +0530

    Update src/main/java/epam/dto/Tag.java

--- src/main/java/epam/dto/Tag.java
+++ src/main/java/epam/dto/Tag.java
@@ -7,7 +7,19 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tag {
+    private Long id;
+    private String name;
+}
     private Long id;
     private String name;
 }
