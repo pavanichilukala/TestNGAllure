8256e3ed417cca946d3e7d2234e34a334d51ba0c
pavanichilukala <73467155+pavanichilukala@users.noreply.github.com>
2025-02-14 13:56:28 +0530

    Create src/main/java/epam/dto/ApiResponse.java

---
+++ src/main/java/epam/dto/ApiResponse.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private Integer code;
+    private String type;
+    private String message;
+}
